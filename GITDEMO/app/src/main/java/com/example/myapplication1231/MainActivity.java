8996bfa621a10651c2ea7312dc52bd722518b3ee
lbm <1>
2021-03-06 21:39:43 +0800

    第2次更新

--- GITDEMO/app/src/main/java/com/example/myapplication1231/MainActivity.java
+++ GITDEMO/app/src/main/java/com/example/myapplication1231/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w("1","!123");
+        Log.w("1","!12311");
     }
 }
